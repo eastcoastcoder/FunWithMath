69dc1bf4442362704c4640232b50288b977e9e8c
ethanx94 <coolkid602007@gmail.com>
2015-05-24 23:59:59 -0500

    Added explanation statements

--- CH0/ElevenMultiplication.rb
+++ CH0/ElevenMultiplication.rb
@@ -1,23 +1,34 @@
 def eleven(multiplicand)
+
+	head = multiplicand[0]
 	tail = multiplicand[1]
+	puts "Head: " + head
+	
 	if multiplicand.length == 1 
 		answer = Integer(multiplicand)*11
 		return String(answer) + " No magic necessary!"
 	elsif multiplicand.length == 2
-		middle = Integer(multiplicand[0]) + Integer(tail)
+		middle = Integer(head) + Integer(tail)
 	else
 		return "Not yet implemented: Multiplicans > 2 in length"
 	end
 
-	if middle < 10 and middle >= 0
+	if middle < 10
+		puts "Middle: " + head + " + " + tail + " = " + String(middle)
 		answer = multiplicand[0] + String(middle) + tail
 	elsif middle >= 10
-		#carry
+		puts head + " + " + tail + " = " + String(middle)
+		
 		head = Integer(multiplicand[0]) + Integer(middle.to_s.split(//).map{|chr| chr.to_i}[0])
+		
+		puts "Carry: " + multiplicand[0] + " + " + String(middle)[0] + " = " + String(head)
+		puts "Middle: " + String(middle)[1]
+		
 		middle = middle.to_s.split(//).map{|chr| chr.to_i}[1]
 		answer =  String(head) + String(middle) + tail
 	end
 	
+	puts "Tail: " + tail
 	return answer
 end
 
