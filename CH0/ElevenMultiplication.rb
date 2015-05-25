b526b7825dbe2f52fcd458a17c2f90ee7311f838
ethanx94 <coolkid602007@gmail.com>
2015-05-24 23:37:05 -0500

    Implemented Multiplicans that sum > 10

--- CH0/ElevenMultiplication.rb
+++ CH0/ElevenMultiplication.rb
@@ -1,22 +1,29 @@
-def eleven (multiplicand)
-	
-	middle = -1
-	if multiplicand.length == 2
-		middle = Integer(multiplicand[0]) + Integer(multiplicand[1])
+def eleven(multiplicand)
+	tail = multiplicand[1]
+	if multiplicand.length == 1 
+		answer = Integer(multiplicand)*11
+		return String(answer) + " No magic necessary!"
+	elsif multiplicand.length == 2
+		middle = Integer(multiplicand[0]) + Integer(tail)
 	else
-		return "Not yet implemented: Multiplicans > 2 in length\n\n"
+		return "Not yet implemented: Multiplicans > 2 in length"
 	end
 
 	if middle < 10 and middle >= 0
-		return multiplicand[0] + String(middle) + multiplicand[1]
-	elsif middle == -1 or middle >= 10
-		return "Not yet implemented: Multiplicans that sum > 10\n\n"
+		answer = multiplicand[0] + String(middle) + tail
+	elsif middle >= 10
+		#carry
+		head = Integer(multiplicand[0]) + Integer(middle.to_s.split(//).map{|chr| chr.to_i}[0])
+		middle = middle.to_s.split(//).map{|chr| chr.to_i}[1]
+		answer =  String(head) + String(middle) + tail
 	end
+	
+	return answer
 end
 
 while true
 	print "Enter a number to be multiplied by 11: \n11*"
 	#chomp removes enter key from length
-	puts eleven(gets.chomp)
+	puts "Answer: " + eleven(gets.chomp) + "\n\n"
 	
 end
