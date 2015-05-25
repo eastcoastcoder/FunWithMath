0d98dbf661831a7f848fca59d71498161102c24d
ethanx94 <coolkid602007@gmail.com>
2015-05-24 23:00:45 -0500

    Defined a method

--- CH0/ElevenMultiplication.rb
+++ CH0/ElevenMultiplication.rb
@@ -1,19 +1,22 @@
-while true
-	print "Enter a number to be multiplied by 11: \n11*"
-	
-	#chomp removes extra character (enter key) from length
-	multiplicand = gets.chomp
+def eleven (multiplicand)
 	
 	middle = -1
 	if multiplicand.length == 2
 		middle = Integer(multiplicand[0]) + Integer(multiplicand[1])
 	else
-		puts "Not yet implemented: Multiplicans > 2 in length\n"
+		return "Not yet implemented: Multiplicans > 2 in length\n\n"
 	end
 
 	if middle < 10 and middle >= 0
-		puts multiplicand[0] + String(middle) + multiplicand[1]
+		return multiplicand[0] + String(middle) + multiplicand[1]
 	elsif middle == -1 or middle >= 10
-		puts "Not yet implemented: Multicands that sum > 10\n\n"
+		return "Not yet implemented: Multiplicans that sum > 10\n\n"
 	end
+end
+
+while true
+	print "Enter a number to be multiplied by 11: \n11*"
+	#chomp removes enter key from length
+	puts eleven(gets.chomp)
+	
 end
